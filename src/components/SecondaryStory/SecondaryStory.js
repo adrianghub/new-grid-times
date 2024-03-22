f8e89e708eddddcad62d263a466f778a4c4a228f
Adrian Zinko <zinko.adrian00@gmail.com>
2024-03-22 18:21:15 +0100

    Update package.json and fix styling in Logo, OpinionStory, MainStory, SecondaryStory, and Header components

--- src/components/SecondaryStory/SecondaryStory.js
+++ src/components/SecondaryStory/SecondaryStory.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
+import {QUERIES} from "../../constants";
 
-const SecondaryStory = ({ id, title, image, location, abstract }) => {
+const SecondaryStory = ({id, title, image, location, abstract}) => {
   return (
     <a href={`/story/${id}`}>
       <Wrapper>
@@ -16,11 +17,19 @@ const SecondaryStory = ({ id, title, image, location, abstract }) => {
 const Wrapper = styled.article`
   display: grid;
   grid-template-areas:
-    'image heading'
-    'image abstract';
+    "image heading"
+    "image abstract";
   gap: 4px 16px;
   grid-template-columns: 120px 1fr;
   color: var(--color-gray-900);
+
+  @media ${QUERIES.tabletOnly} {
+    grid-template-areas:
+      "image"
+      "heading"
+      "abstract";
+    grid-template-columns: 1fr;
+  }
 `;
 
 const Image = styled.img`
@@ -45,6 +54,12 @@ const Abstract = styled.p`
   grid-area: abstract;
   font-size: 1rem;
   white-space: pre-wrap;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  /* Necessary for line clamping */
+  overflow: hidden;
+  align-self: start;
 `;
 
 export default SecondaryStory;
